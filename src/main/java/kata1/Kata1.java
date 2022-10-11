2cbe939ef7756d5f88321fedea7a72b46c55f15d
omar <omar@macbook-pro-de-omar.home>
2022-10-11 16:23:21 +0100

    Commit v2

--- src/main/java/kata1/Kata1.java
+++ src/main/java/kata1/Kata1.java
@@ -1,13 +1,18 @@
 package kata1;
 
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Zacarias", new Date(102, 9, 25));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2002, 9, 24);
         
+        Person person = new Person("Blas", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años ");
     }
 }
